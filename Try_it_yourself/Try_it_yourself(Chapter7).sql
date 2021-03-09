6525974d1963a26201f167fca1fe675320c36699
ConradMare890317 <Conradmare@gmail.com>
2021-03-09 17:58:24 +0200

    2021/03/09 (pm)
    Table Designs Excersise

---
+++ Try_it_yourself/Try_it_yourself(Chapter7).sql
@@ -0,0 +1,37 @@
+CREATE TABLE albums (
+    album_id bigserial,
+    album_catalog_code varchar(100),
+    album_title text,
+    album_artist text,
+    album_time interval,
+    album_release_date date,
+    album_genre varchar(40),
+    album_description text
+);
+
+CREATE TABLE songs (
+    song_id bigserial,
+    song_title text,
+    song_artist text,
+    album_id bigint
+);
+
+CREATE TABLE albums (
+    album_id bigserial,
+    album_catalog_code varchar(100) NOT NULL,
+    album_title text NOT NULL,
+    album_artist text NOT NULL,
+    album_release_date date,
+    album_genre varchar(40),
+    album_description text,
+    CONSTRAINT album_id_key PRIMARY KEY (album_id),
+    CONSTRAINT release_date_check CHECK (album_release_date > '1/1/1925')
+);
+
+CREATE TABLE songs (
+    song_id bigserial,
+    song_title text NOT NULL,
+    song_artist text NOT NULL,
+    album_id bigint REFERENCES albums (album_id),
+    CONSTRAINT song_id_key PRIMARY KEY (song_id)
+);
