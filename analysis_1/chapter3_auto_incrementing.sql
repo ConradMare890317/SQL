957547f842eff35a47c13c2014025e3ec39dcb19
ConradMare890317 <Conradmare@gmail.com>
2021-03-24 17:19:11 +0200

    2021/03/24 (pm)
    Difference of smallserial, serial & bigserial (length of characters)

---
+++ analysis_1/chapter3_auto_incrementing.sql
@@ -0,0 +1,15 @@
+CREATE TABLE people (
+	id serial,
+	person_name varchar (100)
+	--person_name varchar (2147483647)
+);
+
+--CREATE TABLE people (
+	--id smallserial,
+	--person_name varchar (32767)
+--);
+
+--CREATE TABLE people (
+	--id bigserial,
+	--person_name varchar (9223372036854775807)
+--);
