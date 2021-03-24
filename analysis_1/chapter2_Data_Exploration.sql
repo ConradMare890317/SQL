1d659bab96ff0678c1577a23c50dbb34f2a71076
ConradMare890317 <Conradmare@gmail.com>
2021-03-24 11:52:41 +0200

    2021/03/24 (am)
    Chapter 2, data exploration.
    Exploring data from "teachers" in chapter1

---
+++ analysis_1/chapter2_Data_Exploration.sql
@@ -0,0 +1,49 @@
+--SELECT * FROM chapter1_teachers;
+
+--SELECT last_name, first_name, salary FROM chapter1_teachers;
+
+--SELECT DISTINCT school
+--FROM chapter1_teachers;
+
+--SELECT DISTINCT school, salary
+--FROM chapter1_teachers;
+
+--SELECT first_name, last_name, salary
+--FROM chapter1_teachers
+--ORDER BY salary DESC;
+
+--SELECT last_name, school, hire_date
+--FROM chapter1_teachers
+--ORDER BY school ASC, hire_date DESC;
+
+--SELECT last_name, school, hire_date
+--FROM chapter1_teachers
+--WHERE school = 'Myers Middle School';
+
+--SELECT first_name
+--FROM chapter1_teachers
+--WHERE first_name LIKE 'sam%';
+
+--SELECT first_name
+--FROM chapter1_teachers
+--WHERE first_name ILIKE 'sam%';
+
+--SELECT *
+--FROM chapter1_teachers
+--WHERE school = 'Myers Middle School'
+      --AND salary < 40000;
+
+--SELECT *
+--FROM chapter1_teachers
+--WHERE last_name = 'Cole'
+      --OR last_name = 'Bush';
+
+--SELECT *
+--FROM chapter1_teachers
+--WHERE school = 'F.D. Roosevelt HS'
+      --AND (salary < 38000 OR salary > 40000);
+
+SELECT first_name, last_name, school, hire_date, salary
+FROM chapter1_teachers
+WHERE school LIKE '%Roos%'
+ORDER BY hire_date DESC;
