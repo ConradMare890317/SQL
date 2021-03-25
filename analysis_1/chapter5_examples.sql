25c527c6b5081d50461df4a57c443b6acd0195f8
ConradMare890317 <Conradmare@gmail.com>
2021-03-25 17:43:05 +0200

    2021/03/25 (pm)

---
+++ analysis_1/chapter5_examples.sql
@@ -0,0 +1,17 @@
+--INSERT INTO chapter5_basic_math_and_stats_with_sql
+--VALUES(
+	--Examples varchar(100000)
+--);
+
+
+--BASIC MATH AND STATS USING SQL
+--SELECT 2 + 2;
+--SELECT 9 - 1;
+--SELECT 3 * 4;
+
+
+--DIVISION AND MODULO
+--SELECT 11/6;
+--SELECT 11%6;
+--SELECT 11.0/6;
+--SELECT CAST (11 AS numeric(3,1)) / 6;
