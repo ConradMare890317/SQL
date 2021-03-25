e7685d69d22c0d31942942e3dc9a77fb448bafbb
ConradMare890317 <Conradmare@gmail.com>
2021-03-25 11:45:03 +0200

    2021/03/25 (am)

---
+++ analysis_1/chapter4_Importing_and_Exporting_Data.sql
@@ -0,0 +1,37 @@
+--CREATE TABLE chapter4_importing_and_exporting_data (
+--);
+
+--EXAMPLES:
+
+
+--WORKING WITH DELIMITED TEXT FILES
+
+--Typical comma-delimited file data row:
+--John,Doe,123 Main St.,Hyde Park,NY,845-555-1212
+
+--Quoting columns that contain delimiters:
+--John,Doe,"123 Main St., Apartment 200",Hyde Park,NY,845-555-1212
+
+--Handling header rows:
+--FIRSTNAME,LASTNAME,STREET,CITY,STATE,PHONE
+--John,Doe,"123 Main St., Apartment 200",Hyde Park,NY,845-555-1212
+
+
+--USING COPY TO IMPORT DATA
+
+--COPY table_name
+--FROM 'C:\YourDirectory\your_file.csv'
+--WITH (FORMAT CSV, HEADER);
+
+--Input and output file format:
+-- Use 'FORMAT format_name'
+
+--Presence of a header row:
+--Use 'HEADER'
+
+--Delimiter:
+--DELIMITER 'charachter' = A single character you can use to specify wich charachter you use as delimiter
+
+
+--IMPORTING CENSUS DATA DESCRIBING COUNTIES
+--(Creating a new table)
