e43db13804352667cd9097a278bd48b0233c919a
ConradMare890317 <Conradmare@gmail.com>
2021-04-01 19:20:10 +0200

    2021/04/01 (pm)

---
+++ Projects/Teachers_Table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE chapter1_teachers (
+	id bigserial,
+	first_name varchar (30),
+	last_name varchar (30),
+	department_name varchar (30),
+	job_title varchar (30),
+	salary_figure numeric,
+	overtime_hours time
+);
