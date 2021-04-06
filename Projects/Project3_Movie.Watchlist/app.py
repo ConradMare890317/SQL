ec4ef2af72ccd130eb9d7bc995d964cb3f42c345
ConradMare890317 <Conradmare@gmail.com>
2021-04-06 13:05:59 +0200

    2021/04/06 (pm)

---
+++ Projects/Project3_Movie.Watchlist/app.py
@@ -0,0 +1,15 @@
+import database
+
+menu = """Please select one of the following options:
+1) Add new movie.
+2) View upcoming movies.
+3) View all movies.
+4) Watch a movie.
+5) View watched movies.
+6) Exit.
+
+Your selection: """
+welcome - "Welcome to the watchlist app!"
+
+
+print(welcome)
