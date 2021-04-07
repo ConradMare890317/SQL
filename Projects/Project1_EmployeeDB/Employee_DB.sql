32fbea9b4bcc7940c164ff9cf37797678b1ed968
ConradMare890317 <Conradmare@gmail.com>
2021-04-07 11:52:47 +0200

    2021/04/07 (am)

--- Projects/Project1_EmployeeDB/Employee_DB.sql
+++ Projects/Project1_EmployeeDB/Employee_DB.sql
@@ -1,27 +1,27 @@
 CREATE TABLE departments (
-    dept_id bigserial,
-    dept varchar(100),
+    depts_id bigserial,
+    depts varchar(100),
     city varchar(100),
-    CONSTRAINT dept_key PRIMARY KEY (dept_id),
-    CONSTRAINT dept_city_unique UNIQUE (dept, city)
+    CONSTRAINT depts_key PRIMARY KEY (depts_id),
+    CONSTRAINT depts_city_unique UNIQUE (depts, city)
 );
 
 CREATE TABLE employees (
-    emp_id bigserial,
+    emps_id bigserial,
     first_name varchar(100),
     last_name varchar(100),
     salary integer,
-    dept_id integer REFERENCES chapter6_departments (dept_id),
-    CONSTRAINT emp_key PRIMARY KEY (emp_id),
-    CONSTRAINT emp_dept_unique UNIQUE (emp_id, dept_id)
+    depts_id integer REFERENCES departments (depts_id),
+    CONSTRAINT emps_key PRIMARY KEY (emps_id),
+    CONSTRAINT emps_depts_unique UNIQUE (emps_id, depts_id)
 );
 
-INSERT INTO departments (dept, city)
+INSERT INTO departments (depts, city)
 VALUES
     ('Tax', 'Atlanta'),
     ('IT', 'Boston');
 
-INSERT INTO employees (first_name, last_name, salary, dept_id)
+INSERT INTO employees (first_name, last_name, salary, depts_id)
 VALUES
     ('Nancy', 'Jones', 62500, 1),
     ('Lee', 'Smith', 59300, 1),
